430735bf93c4cf46a05db07ef63f31f724ad366b
kanostar <1075434518@qq.com>
2024-10-28 15:17:16 +0800

    p站爬虫2.2

--- pixivCrawl.py
+++ pixivCrawl.py
@@ -35,8 +35,8 @@ def request_if_error(response):
 
 
 # 创建文件夹
-def create_directory(base_dir):
-    mkdir = os.path.join(os.getcwd(), base_dir)
+def create_directory(*base_dir):
+    mkdir = os.path.join(os.getcwd(), *base_dir)
     os.makedirs(mkdir, exist_ok=True)
     return mkdir
 
@@ -95,7 +95,7 @@ class ThroughArtWorkerId:
         request_if_error(response)
         static_url = json.loads(response.text)['body']  # 静态图的url
         if len(static_url) > 1:
-            mkdir_static = create_directory(self.img_id)
+            mkdir_static = create_directory("artworks_IMG", self.img_id)
         logging.info(f"一共{len(static_url)}张插画，正在下载~")
 
         # 计算文件总大小，更新进度条最大值
@@ -175,6 +175,7 @@ class ThroughArtWorkerId:
 # 通过画师id下载
 class ThroughWorkerId:
     def __init__(self, cookie_id, users_id, pixiv_app):
+        self.mkdirs_workers = ""
         self.users_id = users_id
         self.app = pixiv_app
         self.cookie = cookie_id if cookie_id != "" else cookie
@@ -206,8 +207,9 @@ class ThroughWorkerId:
     def preDownload(self):
         try:
             # 如果没有文件夹，那就创建一个
-            mkdirs_workers = os.path.join(os.getcwd(), "workers_IMG", self.getWorkerName())
-            os.makedirs(mkdirs_workers, exist_ok=True)
+            self.mkdirs_workers = create_directory("workers_IMG", self.getWorkerName())
+            # self.mkdirs_workers = os.path.join(os.getcwd(), "workers_IMG", self.getWorkerName())
+            # os.makedirs(self.mkdirs_workers, exist_ok=True)
 
             listId = self.getImgId()
             logging.info(f"大概下载图片数: {len(listId)}+")
@@ -216,18 +218,18 @@ class ThroughWorkerId:
             logging.info("正在检索并下载中，请稍后(进度条会有点延迟)。。。")
             self.app.update_progress_bar(0, len(listId))  # 初始化进度条
             with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-                futures = [executor.submit(self.download_image_or_gif, img_id, mkdirs_workers) for img_id in listId]
+                futures = [executor.submit(self.download_image_or_gif, img_id) for img_id in listId]
                 for future in as_completed(futures):
                     future.result()
                     self.app.update_progress_bar(1)  # 更新进度条
-            logging.info(f"下载完成，共有{len(os.listdir(mkdirs_workers))}张图片~")
+            logging.info(f"下载完成，共有{len(os.listdir(self.mkdirs_workers))}张图片~")
         except TypeError:
             logging.warning("输入的画师id有误或不存在，也可能是你cookie失效了，换一个吧~")
         except Exception as e:
             logging.critical(f"发生了一个错误: {e}")
 
     # 下载静态图片或动图
-    def download_image_or_gif(self, img_id, mkdirs):
+    def download_image_or_gif(self, img_id):
         time.sleep(random.uniform(1, 5))
         ugoira_url = f"{self.url_illust}/{img_id}/ugoira_meta"
         session = requests.Session()
@@ -236,12 +238,12 @@ class ThroughWorkerId:
 
         # 判断是否为动图
         if data['error']:
-            self.download_static_images(session, img_id, mkdirs)
+            self.download_static_images(session, img_id)
         else:
-            self.download_ugoira(data, img_id, mkdirs)
+            self.download_ugoira(data, img_id)
 
     # 下载静态图片
-    def download_static_images(self, session, img_id, mkdir_static):
+    def download_static_images(self, session, img_id):
         response = session.get(url=f"{self.url_illust}/{img_id}/pages", verify=False, headers=self.headers)
         request_if_error(response)
         static_url = json.loads(response.text)['body']
@@ -251,14 +253,14 @@ class ThroughWorkerId:
             futures = []
             for ID in static_url:
                 href1 = ID['urls']['original']
-                futures.append(executor.submit(self.download_image, href1, mkdir_static))
+                futures.append(executor.submit(self.download_image, href1))
 
             # 等待所有任务完成
             for future in as_completed(futures):
                 future.result()
 
-    def download_image(self, url, mkdirs):
-        file_path = os.path.join(mkdirs, f"@{self.getWorkerName()} {os.path.basename(url)}")
+    def download_image(self, url):
+        file_path = os.path.join(self.mkdirs_workers, f"@{self.getWorkerName()} {os.path.basename(url)}")
         session = requests.Session()
         download_response = session.get(url=url, headers=self.headers, verify=False, stream=True)
         request_if_error(download_response)
@@ -267,15 +269,15 @@ class ThroughWorkerId:
                 f.write(data)
 
     # 下载动图
-    def download_ugoira(self, data, img_id, mkdirs):
+    def download_ugoira(self, data, img_id):
         delays = [frame['delay'] for frame in data['body']['frames']]
         with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-            futures = [executor.submit(self.download_gif, data['body']['originalSrc'], img_id, mkdirs, delays)]
+            futures = [executor.submit(self.download_gif, data['body']['originalSrc'], img_id, delays)]
             for future in as_completed(futures):
                 future.result()
 
-    def download_gif(self, url, img_id, mkdirGif, delays):
-        file_path = os.path.join(mkdirGif, f"@{self.getWorkerName()} {img_id}.gif")
+    def download_gif(self, url, img_id, delays):
+        file_path = os.path.join(self.mkdirs_workers, f"@{self.getWorkerName()} {img_id}.gif")
         response = requests.get(url, headers=self.headers, verify=False)
         request_if_error(response)
         zip_content = io.BytesIO(response.content)
@@ -284,7 +286,6 @@ class ThroughWorkerId:
             image_files = [f for f in zip_ref.namelist() if f.endswith(('.png', '.jpg', '.jpeg'))]
             image_files.sort()
 
-            update_threshold = 1024 * 1024
             images = []
             for image_file in image_files:
                 with zip_ref.open(image_file) as image_file_obj:
@@ -458,7 +459,8 @@ class PixivApp:
 
 
 if __name__ == '__main__':
-    cookie = "first_visit_datetime_pc=2024-03-25%2023%3A22%3A07; p_ab_id=7; p_ab_id_2=4; p_ab_d_id=170919518; yuid_b=NWcCYEc; c_type=31; privacy_policy_notification=0; a_type=0; b_type=1; login_ever=yes; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^6=user_id=55796473=1^9=p_ab_id=7=1^10=p_ab_id_2=4=1^11=lang=zh=1; privacy_policy_agreement=7; first_visit_datetime=2024-09-28%2022%3A53%3A28; webp_available=1; _ga_3WKBFJLFCP=GS1.1.1727531608.1.0.1727531610.0.0.0; _gid=GA1.2.259876661.1729870123; __utma=235335808.2067325330.1711376549.1712414350.1730007067.2; __utmz=235335808.1730007067.2.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _gcl_au=1.1.1599279373.1730037390; device_token=cfd3583f48fcd27e771de19dda7cc785; __cf_bm=1A.CeFXLk6V.UymC4pAK0u4fWeZ7u3Rct4DbvzS4egY-1730037583-1.0.1.1-s3YCP0aq658h6dAczfwGJU6LOM5xvRdy5nueuJ_dGx4WH7LoQ9ccbEDKetl9zSSNa7QfW889l_c.Zo.KYBw_jxWBnd2r7bG0NWAYCW7hSMA; cf_clearance=ls8LDfx_yN8ubL7X.RHHELyEyKtMxE_N42rsJ8MK40k-1730037584-1.2.1.1-XYaeLESjNYukegE6QpScxSizjcAy0Qj_gpui6oXC.pr8NaLyPqZGVyKo9ZQcTq4TzJrn7UL4uOPGPsvUpO3636dKYwqRHXygAfkgrrPwce91sF0V7HXBf1IsYC.2bg0SIfiFuxuDEI04Miu0ILJ8h1i9ZCJrBuVJC3da8oaE24IocjlYLanscJndzjSeuwWa_IJqtycXiVdBXO4Q6_e4RNOFM_kVoe23qNHT5Y7gRZKNe0v0YXaaGC2rTKHkKoVCsSJ0Z14jp5m1FsfGgFubxM1A3S1zWg.NEJCTAnpuWHHjTaFhXAbMfqg7KmdrvLVOFapXaA5SSLzdszt0xdB8LFDzposXH_qLB3NThtmk6o.bT.tNtjJARTwoFOiX2LTVDeH8b0pNiMtxBzsG_NFsIQ; cc1=2024-10-27%2023%3A05%3A51; PHPSESSID=55796473_m3c9lT52U2ZCgamnHWBCOdXUDVfKRGYS; _ga_MZ1NL4PHH0=GS1.1.1730037393.10.1.1730037965.0.0.0; _ga=GA1.2.2067325330.1711376549; _gat_UA-1830249-3=1; _ga_75BBYNYN9J=GS1.1.1730035841.61.1.1730038499.0.0.0"
+    cookie = "first_visit_datetime_pc=2024-10-28%2015%3A47%3A50; cc1=2024-10-28%2015%3A47%3A50; yuid_b=MDhIaSQ; p_ab_id=0; p_ab_id_2=5; p_ab_d_id=1010825385; _gid=GA1.2.304787948.1730098076; __cf_bm=fZzbZtN_g0ZDTf9n52iWLMFU1WEmnH81VoV8Wd0rEDc-1730098595-1.0.1.1-Xl9cDP.mcn0FfInILgvhzFksOEa9XkK1y.8aRkk9Q17sX3wOkYRaZVYa.zMZyrSqEqSLMdPG_JuZ6drTARrK8SZAIoSmfnz7q3MC5bo5OcI; cf_clearance=ItF6dTW2176VOIpbIzYu7nsHOq3SvOKTrhUwMLVxIkw-1730098596-1.2.1.1-NT_Me9C2T7ME8DKrqu27..qbQVeH07eHpdrfJV.Rgl0wTK9QWA2x2vf.sfSa8VTTzj1yaeQeEFt1LwNNg1_EtMhQndt7p_Lfc6yeuLOROXhLref_.iHu.xKJ19NQe7ziH0sJXQyHDhnGKNdkV6TooYGK8MtVIfIpz1DKovd6jQBtGDS1PAC4G.hNugmNSF0vB6zcMvuUvQ2RztdZIAYHGTK2X1Eu2PVUoS6AP_ee01M..q2NH49wfDO.m6pJZgvHwvvdbLa5AxGwRuXvRt5_QZT2zWy5R5sF7.B59W8.JBekKbBHVZxazp11erzDwmmeuvKr_xtDFVscXQRNfxjs38T9FFumAchzGA_catoR8xRErCL4YEi5pmo3aFSnmXYq8p2SOY2WowQwGv7ePWtd4A; PHPSESSID=55796473_fl4Q8QxUzGAGEZPTJTTZBGlHp9S52OVF; device_token=f8e45aad75360f69b925b28a89135872; privacy_policy_agreement=7; _ga_MZ1NL4PHH0=GS1.1.1730098551.1.1.1730098603.0.0.0; c_type=31; privacy_policy_notification=0; a_type=0; b_type=1; login_ever=yes; _ga=GA1.1.1043447830.1730098071; _ga_75BBYNYN9J=GS1.1.1730098070.1.1.1730099005.0.0.0"
+    # cookie = "first_visit_datetime_pc=2024-03-25%2023%3A22%3A07; p_ab_id=7; p_ab_id_2=4; p_ab_d_id=170919518; yuid_b=NWcCYEc; c_type=31; privacy_policy_notification=0; a_type=0; b_type=1; login_ever=yes; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^6=user_id=55796473=1^9=p_ab_id=7=1^10=p_ab_id_2=4=1^11=lang=zh=1; privacy_policy_agreement=7; first_visit_datetime=2024-09-28%2022%3A53%3A28; webp_available=1; _ga_3WKBFJLFCP=GS1.1.1727531608.1.0.1727531610.0.0.0; _gid=GA1.2.259876661.1729870123; __utma=235335808.2067325330.1711376549.1712414350.1730007067.2; __utmz=235335808.1730007067.2.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _gcl_au=1.1.1599279373.1730037390; device_token=cfd3583f48fcd27e771de19dda7cc785; __cf_bm=1A.CeFXLk6V.UymC4pAK0u4fWeZ7u3Rct4DbvzS4egY-1730037583-1.0.1.1-s3YCP0aq658h6dAczfwGJU6LOM5xvRdy5nueuJ_dGx4WH7LoQ9ccbEDKetl9zSSNa7QfW889l_c.Zo.KYBw_jxWBnd2r7bG0NWAYCW7hSMA; cf_clearance=ls8LDfx_yN8ubL7X.RHHELyEyKtMxE_N42rsJ8MK40k-1730037584-1.2.1.1-XYaeLESjNYukegE6QpScxSizjcAy0Qj_gpui6oXC.pr8NaLyPqZGVyKo9ZQcTq4TzJrn7UL4uOPGPsvUpO3636dKYwqRHXygAfkgrrPwce91sF0V7HXBf1IsYC.2bg0SIfiFuxuDEI04Miu0ILJ8h1i9ZCJrBuVJC3da8oaE24IocjlYLanscJndzjSeuwWa_IJqtycXiVdBXO4Q6_e4RNOFM_kVoe23qNHT5Y7gRZKNe0v0YXaaGC2rTKHkKoVCsSJ0Z14jp5m1FsfGgFubxM1A3S1zWg.NEJCTAnpuWHHjTaFhXAbMfqg7KmdrvLVOFapXaA5SSLzdszt0xdB8LFDzposXH_qLB3NThtmk6o.bT.tNtjJARTwoFOiX2LTVDeH8b0pNiMtxBzsG_NFsIQ; cc1=2024-10-27%2023%3A05%3A51; PHPSESSID=55796473_m3c9lT52U2ZCgamnHWBCOdXUDVfKRGYS; _ga_MZ1NL4PHH0=GS1.1.1730037393.10.1.1730037965.0.0.0; _ga=GA1.2.2067325330.1711376549; _gat_UA-1830249-3=1; _ga_75BBYNYN9J=GS1.1.1730035841.61.1.1730038499.0.0.0"
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
     root = Tk()
     app = PixivApp(root)
